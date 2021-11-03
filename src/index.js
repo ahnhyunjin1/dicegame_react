ddc66fd6e86807d5289b03fa85cdb95ed18bdb8c
ahnhyunjin1 <a01020813765@gmail.com>
2021-11-03 18:49:52 +0900

    message

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+
+
+function handleClick() {
+  console.log("가위바위보!");
+}
+
+
+ReactDOM.render(
+  <>
+    <h1>가위바위보</h1>
+    <h2>승리</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>,
+  document.getElementById('root')
+);
