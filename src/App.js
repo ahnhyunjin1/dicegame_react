9c9771d5d00fe459e4836478986d459b0a4d8d45
ahnhyunjin1 <a01020813765@gmail.com>
2021-11-08 15:20:15 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Button from './button';
 import Dice from './Dice';
 
 function App() {
     return (
         <div>
+            <div>
+                <Button text="던지기" />
+                <Button text="처음부터" />
+            </div>
             <Dice color="red" num={2} />
         </div>
     );
